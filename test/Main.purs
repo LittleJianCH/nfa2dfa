0a5a11e50bfc6276b478e4973f0a7ae89b5205aa
简 <2447746158@qq.com>
2022-10-18 08:32:37 +0800

    add more tests

--- test/Main.purs
+++ test/Main.purs
@@ -1,14 +1,14 @@
 module Test.Main where
 
-import Data.Array
-import Prelude
-
-import Data.Foldable (class Foldable)
+import Prelude (Unit, discard, ($), (==))
+import Data.Array (all)
 import Data.Map as M
 import Data.Set as S
-import Data.String.CodeUnits (toCharArray)
 import Data.Tuple.Nested ((/\))
-import Definitions.DFA (DFA, recognize, recognizeStr)
+import Definitions.DFA (DFA)
+import Definitions.DFA as D
+import Definitions.NFA (NFA)
+import Definitions.NFA as N
 import Effect (Effect)
 import Effect.Console (log)
 import Test.Assert (assert)
@@ -27,8 +27,60 @@ dfa1 = {
   ]
 }
 
+dfa2 :: DFA Int
+-- dfa2 is an DFA that accepts the language which
+-- is ended with 0 and has odd length
+-- or is ended with 1 and has even length
+dfa2 = {
+  start : 1,
+  accpects : S.fromFoldable [2, 5],
+  transition : M.fromFoldable [
+    (1 /\ '0') /\ 2,
+    (1 /\ '1') /\ 4,
+    (2 /\ '0') /\ 3,
+    (2 /\ '1') /\ 5,
+    (3 /\ '0') /\ 2,
+    (3 /\ '1') /\ 4,
+    (4 /\ '0') /\ 3,
+    (4 /\ '1') /\ 5,
+    (5 /\ '0') /\ 2,
+    (5 /\ '1') /\ 4
+  ]
+}
+
+nfa1 :: NFA Int
+-- nfa1 is an NFA that accepts the language which
+-- is ended with 0 and has odd length
+-- or is ended with 1 and has even length
+nfa1 = {
+  starts : S.fromFoldable [1],
+  accpects : S.fromFoldable [3, 4],
+  transition : M.fromFoldable [
+    (1 /\ '0') /\ S.fromFoldable [2, 3],
+    (1 /\ '1') /\ S.fromFoldable [2],
+    (2 /\ '0') /\ S.fromFoldable [1],
+    (2 /\ '1') /\ S.fromFoldable [1, 4]
+  ]
+}
+
 main :: Effect Unit
 main = do
-  assert $ recognizeStr dfa1 "0001" == true
-  assert $ recognizeStr dfa1 "1001" == false
+  assert $ D.recognizeStr dfa1 "0001" == true
+  assert $ D.recognizeStr dfa1 "1001" == false
+  assert $ D.recognizeStr dfa1 "0aa1" == false
+
+  let zeroOddOneEvenTests = [
+    "0" /\ true,
+    "11" /\ true,
+    "01" /\ true,
+    "0100" /\ false,
+    "101" /\ false
+  ]
+  assert $ all
+    (\(str /\ expected) -> D.recognizeStr dfa2 str == expected)
+    zeroOddOneEvenTests
+  assert $ all
+    (\(str /\ expected) -> N.recognizeStr nfa1 str == expected)
+    zeroOddOneEvenTests
+
   log "All test passed 🎉🎉🎉"
