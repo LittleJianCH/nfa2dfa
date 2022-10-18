cd70fd168116984e87754a36e1a8bba456d4f553
简 <2447746158@qq.com>
2022-10-18 19:56:29 +0800

    setup webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const element = document.createElement("h1");
+
+element.innerHTML = "Welcome to NFA2DFA!";
+
+document.body.appendChild(element);
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
+};
